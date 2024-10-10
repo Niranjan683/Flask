5edee65a1d496cd13e07a96c56a30fb6d2f5088b
unknown <niranjan20032021@gmail.com>
2024-10-10 21:43:53 +0530

    basic understaing of flask

---
+++ flask/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+'''
+It creates an instance of the Flask class,
+which will be your WSGI (Web Server Gateway Interface) application,
+'''
+app=Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Welcome to home page."
+@app.route('/index')
+def index():
+    return "welcome to index page"
+
+
+if __name__=="__main__":
+    app.run(debug=True)
